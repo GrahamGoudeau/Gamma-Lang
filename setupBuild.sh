a77e76d95719c76164014f8b76fcd98ab401af7e
Graham Goudeau <grahamgoudeau@gmail.com>
2016-08-14 17:30:12 -0400

    Updated the build process to handle file name and line number directives

---
+++ setupBuild.sh
@@ -0,0 +1,19 @@
+buildDir=build/
+srcDir=src/
+if [ ! -d "$buildDir" ]; then
+    mkdir $buildDir
+fi
+
+rm "$buildDir"*
+
+lineDirective=__LINE__
+fileDirective=__FILE__
+for fileName in "$srcDir"*; do
+    buildDestination=$buildDir$(echo $fileName | sed "s|$srcDir||")
+    sedCmd="s|$fileDirective|\"$fileName\"|"
+
+    # replace each instance of $lineDirective with the current line in the file
+    # then replace $fileDirective with the current file name
+    awk "/$lineDirective/{gsub(/$lineDirective/,NR)}1" $fileName |
+        sed $sedCmd >$buildDestination
+done
